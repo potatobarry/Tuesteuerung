fedd1aed17fc95f1d3dd63ae4ab83993d8247973
ZippeP4L <77888029+ZippeP4L@users.noreply.github.com>
2021-08-30 20:10:14 +0200

    + Automatik/Hand/Aus

--- DoorControl.cpp
+++ DoorControl.cpp
@@ -11,11 +11,9 @@
 #include "DoorControl.h"
 #include "Device.hpp"
 
-
-
 DoorControl::DoorControl() : door_if(DoorInterface::get_instance())
 {
-	Device dev;		
+	Device dev;
 }
 
 DoorControl::~DoorControl()
@@ -26,13 +24,102 @@ DoorControl::~DoorControl()
 
 void DoorControl::run()
 {
-	while(!door_if.quit_doorcontrol_flag){
+	//Automatik Modus
+	int s = 0;				   //Automatik boot variable
+	while (BW1 && BW2 == true) //Automatik Modus
+	{
+		bool AZ = false; //Admin ZU
+
+		if (s < 1)
+		{
+			Y3 = true;
+			waitms(5000); //warte 5 sekunden
+			Y3 = false;
+			s++;
+		}
+
+		if (NTZ == true)
+		{
+			AZ = true;
+		}
+
+		if ((NTA || LSH || LSV || BM) == true) //Wenn sensor ausgelöst; Lampe an, fahre auf
+		{
+			Y3 = true;					  //warnlampe an
+			while ((ELO || NTZ) == false) //fahre auf, bis endstop erreicht oder bis knopf gedrückt
+			{
+				Y1 = true;				 //fahre auf
+				if (BW1 && BW2 == false) //break bedingung moduswechsel
+				{
+					Y1 = false;
+					Y3 = false;
+					break;
+				}
+			}
+			Y1 = false;
+			while (int i = 0 < 3000) // warte 3 sekunden, überprüfe jede 0,1 sek pb breakbedingungen erfüllt wurden
+			{
+				if ((!BW1 || !BW2 || NTZ) == true) //break bedingung moduswechsel
+				{
+					Y1 = false;
+					Y3 = false;
+					break;
+				}
+				waitms(100);
+				i = i + 100;
+			}
+			Y3 = false;
+		}
+
+		if ((NTA || LSH || LSV || BM || !AZ) == false) //Fahre zu
+		{
+			while ((NTA || LSH || LSV || BM || ELG || !BW1 || !BW2)) == false)
+				{
+					Y2 = true;
+					Y3 = true;
+				}
+			Y2 = false;
+			Y3 = false;
+			AZ = false;
+		}
+	}
+
+	//Handbetrieb
+	while (BW1 == true && BW2 == false) //Schleife für handbetrieb
+	{
+		Y1 = false;
+		Y2 = false;
+		Y3 = false;
+		while (NTA == true && NTG == false && !ELO) //Befehl fahre auf (taste auf) und endlage offen nicht erreicht
+		{
+			Y1 = true;
+			Y3 = true;
+			if (BW1 && !BW2 == false) //break bedingung moduswechsel
+			{
+				Y1 = false;
+				Y3 = false;
+				break;
+			}
+		}
+		while (NTA == false && NTG == true && !ELG) //befehl fahre zu (taste zu gedrückt, endlage nicht erreicht)
+		{
+			Y2 = true;
+			Y3 = true;
+			if (BW1 && !BW2 == false) //break bedingung moduswechsel
+			{
+				Y2 = false;
+				Y3 = false;
+				break;
+			}
+		}
+	}
 	
-		dev.set_status("Y1", dev.get_status("BW1"));
-		dev.set_status("Y2", dev.get_status("BW2"));
-		dev.set_status("Y3", dev.get_status("NTA"));
-		
+
+	while (!door_if.quit_doorcontrol_flag)
+	{
 		usleep(20 * 1000);
 
+		DoorInterface &door = DoorInterface::get_instance();
+		door.DebugString("Status BW1: " + std::to_string(dev.get_status("BW1")) + "\n");
 	}
 }
